19aa00d5c29127311c6253f1591fd95930c15a61
Unknown <gustaw5022@gmail.com>
2018-10-25 16:36:25 +0200

    dodano mozliwosc rysowania jajka przy pomocy punktow, linii i trojkatow

--- Jajko - 3D/Jajko - 3D/Source.cpp
+++ Jajko - 3D/Jajko - 3D/Source.cpp
@@ -8,13 +8,165 @@
 #include <windows.h>
 #include <gl/gl.h>
 #include <gl/glut.h>
+#include <cmath>
+#include <ctime>
 
+#define M_PI           3.14159265358979323846
 
 typedef float point3[3];
 
 /*************************************************************************************/
 
 // Funkcja rysuj¹ca osie uk³adu wspó³rzêdnych
+
+struct Point
+{
+	float x;
+	float y;
+	float z;
+};
+
+
+//------------ZMIENNE GLOBALNE ------------------------//
+int N = 20;			//wielkosc tablicy
+
+Point **tablica;		//dynamiczna tablica struktur punktowych
+
+Point **kolory;			//dynamiczna tablica kolorów
+
+int model = 1;  // 1- punkty, 2- siatka, 3 - wype³nione trójk¹ty
+
+
+
+void DrawEggPoints()
+{
+	glBegin(GL_POINTS);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			glVertex3f(tablica[i][j].x, tablica[i][j].y - 5.0f, tablica[i][j].z);
+	glEnd();
+}
+
+void DrawEggLines()
+{
+	
+	for (int i = 0; i < N; i++)
+	{
+		glBegin(GL_LINE_STRIP);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		for (int j = 0; j < N; j++)
+			glVertex3f(tablica[i][j].x, tablica[i][j].y - 5.0f, tablica[i][j].z);
+		glEnd();
+
+		glBegin(GL_LINE_STRIP);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		for (int j = 0; j < N; j++)
+			glVertex3f(tablica[j][i].x, tablica[j][i].y - 5.0f, tablica[j][i].z);
+		glEnd();
+
+	}
+		
+
+}
+
+void DrawEggTriangle()
+{
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+		{
+			if(i < (N - 1))
+				if(j < (N - 1))
+				{
+				glBegin(GL_TRIANGLES);
+				glColor3f(kolory[i][j].x, kolory[i][j].y, kolory[i][j].z);
+				glVertex3f(tablica[i][j].x, tablica[i][j].y - 5.0f, tablica[i][j].z);
+				glColor3f(kolory[i + 1][j + 1].x, kolory[i + 1][j + 1].y, kolory[i + 1][j + 1].z);
+				glVertex3f(tablica[i + 1][j+1].x, tablica[i + 1][j + 1].y - 5.0f, tablica[i + 1][j + 1].z);
+				glColor3f(kolory[i][j+1].x, kolory[i][j + 1].y, kolory[i][j + 1].z);
+				glVertex3f(tablica[i][j + 1].x, tablica[i][j + 1].y - 5.0f, tablica[i][j + 1].z);
+				glEnd();
+				glBegin(GL_TRIANGLES);
+				glColor3f(kolory[i][j].x, kolory[i][j].y, kolory[i][j].z);
+				glVertex3f(tablica[i][j].x, tablica[i][j].y - 5.0f, tablica[i][j].z);
+				glColor3f(kolory[i+1][j].x, kolory[i+1][j].y, kolory[i+1][j].z);
+				glVertex3f(tablica[i + 1][j].x, tablica[i + 1][j].y - 5.0f, tablica[i + 1][j].z);
+				glColor3f(kolory[i+1][j+1].x, kolory[i+1][j+1].y, kolory[i+1][j+1].z);
+				glVertex3f(tablica[i+1][j + 1].x, tablica[i+1][j + 1].y - 5.0f, tablica[i+1][j + 1].z);
+				glEnd();
+
+				}
+				else 
+				{
+					glBegin(GL_TRIANGLES);
+					glColor3f(kolory[i][j].x, kolory[i][j].y, kolory[i][j].z);
+					glVertex3f(tablica[i][j].x, tablica[i][j].y - 5.0f, tablica[i][j].z);
+					glColor3f(kolory[0][0].x, kolory[0][0].y, kolory[0][0].z);
+					glVertex3f(tablica[0][0].x, tablica[0][0].y - 5.0f, tablica[0][0].z);
+					glColor3f(kolory[i][0].x, kolory[i][0].y, kolory[i][0].z);
+					glVertex3f(tablica[i][0].x, tablica[i][0].y - 5.0f, tablica[i][0].z);
+					glEnd();
+					
+					glBegin(GL_TRIANGLES);
+					glColor3f(kolory[i][j].x, kolory[i][j].y, kolory[i][j].z);
+					glVertex3f(tablica[i][j].x, tablica[i][j].y - 5.0f, tablica[i][j].z);
+					glColor3f(kolory[0][j].x, kolory[0][j].y, kolory[0][j].z);
+					glVertex3f(tablica[0][j].x, tablica[0][j].y - 5.0f, tablica[0][j].z);
+					glColor3f(kolory[0][0].x, kolory[0][0].y, kolory[0][0].z);
+					glVertex3f(tablica[0][0].x, tablica[0][0].y - 5.0f, tablica[0][0].z);
+					glEnd();
+
+				}
+		}
+}
+
+void GeneratingColors()
+{
+	kolory = new Point*[N];
+	for (int i = 0; i < N; i++)
+		kolory[i] = new Point[N];
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+		{
+			kolory[i][j].x = (float)(rand() % 1000 / 1000.0);
+			kolory[i][j].y = (float)(rand() % 1000 / 1000.0);
+			kolory[i][j].z = (float)(rand() % 1000 / 1000.0);
+		}
+}
+
+void Egg()
+{
+
+	tablica = new Point*[N];
+	for (int i = 0; i < N; i++)
+		tablica[i] = new Point[N];
+	
+	for(int i = 0; i < N; i++)
+		for(int j = 0; j < N; j++)
+		{
+			float u = (float)i / (float)N;
+			float v = (float)j / (float)N;
+			tablica[i][j].x = ((-90 * pow(u, 5) + 225 * pow(u, 4) - 270 * pow(u, 3) + 180 * u*u - 45 * u)*cos((float)M_PI*v));
+			tablica[i][j].y = (160 * pow(u, 4) - 320 * pow(u, 3) + 160 * u*u);
+			tablica[i][j].z = ((-90 * pow(u, 5) + 225 * pow(u, 4) - 270 * pow(u, 3) + 180 * u*u - 45 * u)*sin((float)M_PI*v));
+		}
+
+	switch(model)
+	{
+	case 1:
+		DrawEggPoints();
+		break;
+	case 2:
+		DrawEggLines();
+		break;
+	case 3:
+		DrawEggTriangle();
+		break;
+	default:
+		break;
+	}
+}
 
 
 
@@ -71,6 +223,16 @@ void RenderScene(void)
 	// Czyszczenie macierzy bie¿¹cej
 	Axes();
 	// Narysowanie osi przy pomocy funkcji zdefiniowanej wy¿ej
+/*	glColor3f(1.0f, 1.0f, 1.0f); // Ustawienie koloru rysowania na bia³y
+
+	glRotated(60.0, 1.0, 1.0, 1.0);  // Obrót o 60 stopni
+
+	glutWireTeapot(3.0); // Narysowanie obrazu czajnika do herbaty
+	*/
+
+	//glRotated(35.0, 1.0, 1.0, 1.0);  // Obrót o 60 stopni
+	Egg();
+
 	glFlush();
 	// Przekazanie poleceñ rysuj¹cych do wykonania
 
@@ -83,7 +245,14 @@ void RenderScene(void)
 
 // Funkcja ustalaj¹ca stan renderowania
 
+void keys(unsigned char key, int x, int y)
+{
+	if (key == 'p') model = 1;
+	if (key == 'w') model = 2;
+	if (key == 's') model = 3;
 
+	RenderScene(); // przerysowanie obrazu sceny
+}
 
 void MyInit(void)
 {
@@ -145,12 +314,17 @@ void ChangeSize(GLsizei horizontal, GLsizei vertical)
 
 void main(void)
 {
+	srand(time(NULL));
+
+	GeneratingColors();
 
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
-	glutInitWindowSize(300, 300);
+	glutInitWindowSize(500, 500);
 
 	glutCreateWindow("Uk³ad wspó³rzêdnych 3-D");
+
+	glutKeyboardFunc(keys);
 
 	glutDisplayFunc(RenderScene);
 	// Okreœlenie, ¿e funkcja RenderScene bêdzie funkcj¹ zwrotn¹
@@ -167,6 +341,14 @@ void main(void)
 
 	glutMainLoop();
 	// Funkcja uruchamia szkielet biblioteki GLUT
+
+	for (int i = 0; i < N; i++)
+		delete[] tablica[i];
+	delete[] tablica;
+
+	for (int i = 0; i < N; i++)
+		delete[] kolory[i];
+	delete[] kolory;
 }
 
 /*************************************************************************************/

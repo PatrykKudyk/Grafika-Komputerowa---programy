e1cc67444eaef0ded9b84571650344144e2beaec
Unknown <gustaw5022@gmail.com>
2018-11-21 22:14:12 +0100

    Initial Init with previous code of 3Dtriangle

---
+++ TrójkątSierpińskiego3D-Viewer/TrójkątSierpińskiego3D-Viewer/Source.cpp
@@ -0,0 +1,604 @@
+/*************************************************************************************/
+
+//  Szkielet programu do tworzenia modelu sceny 3-D z wizualizacj¹ osi  
+//  uk³adu wspó³rzednych
+
+/*************************************************************************************/
+
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glut.h>
+#include <ctime>
+#include <iostream>
+
+/*************************************************************************************/
+typedef float point3[3];
+// Funkcja rysuj¹ca osie uk³adu wspó³rzêdnych
+
+struct Point
+{
+	float x;
+	float y;
+	float z;
+};
+
+static GLfloat theta[] = { 0.0, 0.0, 0.0 }; // trzy k¹ty obrotu
+
+float wartosc = -0.015;
+//------------ZMIENNE GLOBALNE ------------------------//
+
+int maxLevel = 0;		//maksymalny poziom algorytmu
+
+int spin = 0;			//okresla kierunek obrotu piramidy
+int stop = 0;
+
+static float startSideLength = 10.0; //dlugosc boku pierwszego ostroslupa
+
+Point startPoint = { { -5.0 } ,{ -2.5 },{ -5.0 } };
+//Point startPoint = { {((-N)/2.0)}, {(-sqrt(startSideLength*(startSideLength / 2.0)))} , {((-N)/2.0)}};
+
+int model = 1;  // 1- czerwony, 2- niebieski, 3 - zielony, 4 - fioletowy
+
+
+
+void Axes(void)
+{
+
+	point3  x_min = { -5.0, 0.0, 0.0 };
+	point3  x_max = { 5.0, 0.0, 0.0 };
+	// pocz¹tek i koniec obrazu osi x
+
+	point3  y_min = { 0.0, -5.0, 0.0 };
+	point3  y_max = { 0.0,  5.0, 0.0 };
+	// pocz¹tek i koniec obrazu osi y
+
+	point3  z_min = { 0.0, 0.0, -5.0 };
+	point3  z_max = { 0.0, 0.0,  5.0 };
+	//  pocz¹tek i koniec obrazu osi y
+	glColor3f(1.0f, 0.0f, 0.0f);  // kolor rysowania osi - czerwony
+	glBegin(GL_LINES); // rysowanie osi x
+	glVertex3fv(x_min);
+	glVertex3fv(x_max);
+	glEnd();
+
+	glColor3f(0.0f, 1.0f, 0.0f);  // kolor rysowania - zielony
+	glBegin(GL_LINES);  // rysowanie osi y
+
+	glVertex3fv(y_min);
+	glVertex3fv(y_max);
+	glEnd();
+
+	glColor3f(0.0f, 0.0f, 1.0f);  // kolor rysowania - niebieski
+	glBegin(GL_LINES); // rysowanie osi z
+
+	glVertex3fv(z_min);
+	glVertex3fv(z_max);
+	glEnd();
+
+}
+
+void RysowanieCzerwony(Point A, float sideLength)
+{
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.9f, 0.0f, 0.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.8f, 0.0f, 0.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.7f, 0.0f, 0.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.6f, 0.0f, 0.0f);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x, A.y, A.z);
+	glEnd();
+
+
+	glBegin(GL_LINE_STRIP);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z);
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+
+}
+
+void RysowanieZielony(Point A, float sideLength)
+{
+	glBegin(GL_POLYGON);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 0.9f, 0.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 0.8f, 0.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 0.7f, 0.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 0.6f, 0.0f);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x, A.y, A.z);
+	glEnd();
+
+
+	glBegin(GL_LINE_STRIP);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z);
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+}
+
+void RysowanieNiebieski(Point A, float sideLength)
+{
+	glBegin(GL_POLYGON);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 0.0f, 0.9f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 0.0f, 0.8f);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 0.0f, 0.7f);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 0.0f, 0.6f);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x, A.y, A.z);
+	glEnd();
+
+
+	glBegin(GL_LINE_STRIP);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z);
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+}
+
+void RysowanieFioletowy(Point A, float sideLength)
+{
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 0.0f, 1.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.9f, 0.0f, 0.9f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.8f, 0.0f, 0.8f);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.7f, 0.0f, 0.7f);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.6f, 0.0f, 0.6f);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glVertex3f(A.x, A.y, A.z);
+	glEnd();
+
+
+	glBegin(GL_LINE_STRIP);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x, A.y, A.z);
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(A.x, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(A.x, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(A.x + sideLength, A.y, A.z + sideLength);
+	glVertex3f(A.x + (sideLength / 2.0), A.y + sqrt(sideLength*(sideLength / 2.0)), A.z + (sideLength / 2.0));
+	glEnd();
+}
+
+
+void DrawSierpinski(Point A, float sideLength, int level)
+{
+	if (level >= maxLevel)
+	{
+		switch (model)
+		{
+		case 1:
+			RysowanieCzerwony(A, sideLength);
+			break;
+		case 2:
+			RysowanieNiebieski(A, sideLength);
+			break;
+		case 3:
+			RysowanieZielony(A, sideLength);
+			break;
+		case 4:
+			RysowanieFioletowy(A, sideLength);
+			break;
+		default:
+			break;
+		}
+
+
+	}
+	else
+	{
+		sideLength = sideLength / 2.0;
+		level += 1;
+		DrawSierpinski(A, sideLength, level);
+		A.x += sideLength;
+		DrawSierpinski(A, sideLength, level);
+		A.x -= sideLength;
+		A.z += sideLength;
+		DrawSierpinski(A, sideLength, level);
+		A.x += sideLength;
+		DrawSierpinski(A, sideLength, level);
+		A.x -= sideLength;
+		A.z -= sideLength;
+		A.y += sqrt(sideLength*(sideLength / 2.0));
+		A.x += (sideLength / 2.0);
+		A.z += (sideLength / 2.0);
+		DrawSierpinski(A, sideLength, level);
+	}
+
+
+	/*
+	glBegin(GL_LINE_STRIP);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(-5.0, 0.0,-5.0);
+	glVertex3f(5.0, 0.0, -5.0);
+	glVertex3f(5.0, 0.0, 5.0);
+	glVertex3f(-5.0, 0.0, 5.0);
+	glVertex3f(-5.0, 0.0, -5.0);
+
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(-5.0, 0.0, -5.0);
+	glVertex3f(0.0,5.0,0.0);
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(5.0, 0.0, -5.0);
+	glVertex3f(0.0, 5.0, 0.0);
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(5.0, 0.0, 5.0);
+	glVertex3f(0.0, 5.0, 0.0);
+	glEnd();
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(-5.0, 0.0, 5.0);
+	glVertex3f(0.0, 5.0, 0.0);
+	glEnd();
+
+	*/
+
+
+
+
+
+}
+
+
+void spinPyramid()
+{
+	if (stop)
+	{
+		if (spin == 0)
+		{
+			theta[0] += wartosc;
+			if (theta[0] < -30.0)  wartosc = 0.015;
+			if (theta[0] > 0.0) wartosc = -0.015;
+			theta[1] -= 0.25;
+		}
+		else
+		{
+			theta[0] += wartosc;
+			if (theta[0] < -30.0)  wartosc = 0.015;
+			if (theta[0] > 0.0) wartosc = -0.015;
+			theta[1] += 0.25;
+		}
+		glutPostRedisplay(); //odœwie¿enie zawartoœci aktualnego okna
+	}
+}
+/*************************************************************************************/
+
+// Funkcja okreœlaj¹ca co ma byæ rysowane (zawsze wywo³ywana gdy trzeba 
+// przerysowaæ scenê)
+
+
+
+void RenderScene(void)
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Czyszczenie okna aktualnym kolorem czyszcz¹cym
+
+	glLoadIdentity();
+	// Czyszczenie macierzy bie¿¹cej
+	//	Axes();
+	// Narysowanie osi przy pomocy funkcji zdefiniowanej wy¿ej
+
+	//glRotated(-30.0, 1.0, 1.0, 1.0);  // Obrót o 60 stopni
+
+	glRotatef(theta[0], 1.0, 0.0, 0.0);
+
+	glRotatef(theta[1], 0.0, 1.0, 0.0);
+
+	glRotatef(theta[2], 0.0, 0.0, 1.0);
+
+
+	DrawSierpinski(startPoint, startSideLength, 0);
+
+	glFlush();
+	// Przekazanie poleceñ rysuj¹cych do wykonania
+
+
+	glutSwapBuffers();
+	//
+}
+
+/*************************************************************************************/
+
+// Funkcja ustalaj¹ca stan renderowania
+
+void MyInit(void)
+{
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	// Kolor czyszc¹cy (wype³nienia okna) ustawiono na czarny
+
+}
+
+/*************************************************************************************/
+
+// Funkcja ma za zadanie utrzymanie sta³ych proporcji rysowanych 
+// w przypadku zmiany rozmiarów okna.
+// Parametry vertical i horizontal (wysokoœæ i szerokoœæ okna) s¹ 
+// przekazywane do funkcji za ka¿dym razem gdy zmieni siê rozmiar okna.
+
+void keys(unsigned char key, int x, int y)
+{
+	if (key == 'c') model = 1;
+	if (key == 'n') model = 2;
+	if (key == 'z') model = 3;
+	if (key == 'f') model = 4;
+	if (key == '+')
+		if (maxLevel<5)
+			++maxLevel;
+	if (key == '-')
+		if (maxLevel > 0)
+			--maxLevel;
+	if (key == 'q')
+		if (spin == 0)
+			spin = 1;
+		else
+			spin = 0;
+	if (key == ' ')
+		if (stop == 0)
+			stop = 1;
+		else
+			stop = 0;
+
+
+	RenderScene(); // przerysowanie obrazu sceny
+}
+
+
+void ChangeSize(GLsizei horizontal, GLsizei vertical)
+{
+
+	GLfloat AspectRatio;
+	// Deklaracja zmiennej AspectRatio  okreœlaj¹cej proporcjê
+	// wymiarów okna 
+	if (vertical == 0)  // Zabezpieczenie przed dzieleniem przez 0
+		vertical = 1;
+	glViewport(0, 0, horizontal, vertical);
+	// Ustawienie wielkoœciokna okna widoku (viewport)
+	// W tym przypadku od (0,0) do (horizontal, vertical)  
+	glMatrixMode(GL_PROJECTION);
+	// Prze³¹czenie macierzy bie¿¹cej na macierz projekcji 
+	glLoadIdentity();
+	// Czyszcznie macierzy bie¿¹cej            
+	AspectRatio = (GLfloat)horizontal / (GLfloat)vertical;
+	// Wyznaczenie wspó³czynnika  proporcji okna
+	// Gdy okno nie jest kwadratem wymagane jest okreœlenie tak zwanej
+	// przestrzeni ograniczaj¹cej pozwalaj¹cej zachowaæ w³aœciwe
+	// proporcje rysowanego obiektu.
+	// Do okreslenia przestrzeni ograniczj¹cej s³u¿y funkcja
+	// glOrtho(...)            
+	if (horizontal <= vertical)
+
+		glOrtho(-7.5, 7.5, -7.5 / AspectRatio, 7.5 / AspectRatio, 10.0, -10.0);
+	else
+
+		glOrtho(-7.5*AspectRatio, 7.5*AspectRatio, -7.5, 7.5, 10.0, -10.0);
+	glMatrixMode(GL_MODELVIEW);
+	// Prze³¹czenie macierzy bie¿¹cej na macierz widoku modelu                                   
+
+	glLoadIdentity();
+	// Czyszcenie macierzy bie¿¹cej
+}
+
+/*************************************************************************************/
+
+// G³ówny punkt wejœcia programu. Program dzia³a w trybie konsoli
+
+
+
+void main(void)
+{
+	srand(time(NULL));
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+	glutInitWindowSize(500, 500);
+
+	glutCreateWindow("Trojkat sierpinskiego w 3-D");
+
+	glutKeyboardFunc(keys);
+
+	glutIdleFunc(spinPyramid);
+
+	glutDisplayFunc(RenderScene);
+	// Okreœlenie, ¿e funkcja RenderScene bêdzie funkcj¹ zwrotn¹
+	// (callback function).  Bedzie ona wywo³ywana za ka¿dym razem 
+	// gdy zajdzie potrzba przeryswania okna 
+	glutReshapeFunc(ChangeSize);
+	// Dla aktualnego okna ustala funkcjê zwrotn¹ odpowiedzialn¹
+	// zazmiany rozmiaru okna      
+	MyInit();
+	// Funkcja MyInit() (zdefiniowana powy¿ej) wykonuje wszelkie
+	// inicjalizacje konieczne  przed przyst¹pieniem do renderowania
+	glEnable(GL_DEPTH_TEST);
+	// W³¹czenie mechanizmu usuwania powierzchni niewidocznych
+
+	glutMainLoop();
+	// Funkcja uruchamia szkielet biblioteki GLUT
+}
+
+/*************************************************************************************/

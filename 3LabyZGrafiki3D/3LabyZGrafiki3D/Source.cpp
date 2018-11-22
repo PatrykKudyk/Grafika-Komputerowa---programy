b2373d50dfd17cf74f27c207aac78f69bdf96223
Unknown <gustaw5022@gmail.com>
2018-11-22 14:52:31 +0100

    init dla Oświetlenia dla scen 3d

---
+++ 3LabyZGrafiki3D/3LabyZGrafiki3D/Source.cpp
@@ -0,0 +1,651 @@
+/*************************************************************************************/
+
+//  Szkielet programu do tworzenia modelu sceny 3-D z wizualizacj¹ osi  
+//  uk³adu wspó³rzêdnych dla rzutowania perspektywicznego
+
+/*************************************************************************************/
+
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glut.h>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+
+#define M_PI  3.14159265358979323846
+
+typedef float point3[3];
+
+//static GLfloat theta1[] = { 0.0, 0.0, 0.0 }; // trzy k¹ty obrotu
+
+struct Point
+{
+	float x;
+	float y;
+	float z;
+};
+
+static int N = 40;			//wielkosc tablicy
+
+Point **tablica;		//dynamiczna tablica struktur punktowych
+
+Point **kolory;			//dynamiczna tablica kolorów
+
+GLfloat promien = 20.0;
+
+float y = 1.0f;
+
+bool kierunek = true;  //true - dodawanie, false - odejmowanie
+
+GLfloat PHI = 0.0, THETA = 0.0;
+//GLfloat cosPhi = 0.5, sinPhi = 0.5, cosTheta = 0.5, sinTheta = 0.5;
+
+static GLfloat viewer[] = { 0.1, 0.1, 10.0 };
+// inicjalizacja po³o¿enia obserwatora
+
+static GLfloat theta[] = { 0.0, 0.0 };   // k¹t obrotu obiektu
+static GLfloat pix2angleX;     // przelicznik pikseli na stopnie
+static GLfloat pix2angleY;     // przelicznik pikseli na stopnie
+
+static GLint statusL = 0;       // stan klawiszy myszy 
+								// 0 - nie naciœniêto ¿adnego klawisza
+								// 1 - naciœniêty zosta³ lewy klawisz
+
+static GLint statusP = 0;       // stan klawiszy myszy 
+								// 0 - nie naciœniêto ¿adnego klawisza
+								// 1 - naciœniêty zosta³ prawy klawisz
+
+static int x_pos_old = 0;       // poprzednia pozycja kursora myszy
+
+static int y_pos_old = 0;       // poprzednia pozycja kursora myszy
+
+static int delta_x = 0;        // ró¿nica pomiêdzy pozycj¹ bie¿¹c¹
+							   // i poprzedni¹ kursora myszy 
+
+static int delta_y = 0;        // ró¿nica pomiêdzy pozycj¹ bie¿¹c¹
+							   // i poprzedni¹ kursora myszy 
+
+							   /*************************************************************************************/
+
+							   /*************************************************************************************/
+							   // Funkcja "bada" stan myszy i ustawia wartoœci odpowiednich zmiennych globalnych
+
+void Mouse(int btn, int state, int x, int y)
+{
+
+
+	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		x_pos_old = x;         // przypisanie aktualnie odczytanej pozycji kursora 
+							   // jako pozycji poprzedniej
+		y_pos_old = y;         // przypisanie aktualnie odczytanej pozycji kursora 
+		statusL = 1;          // wciêniêty zosta³ lewy klawisz myszy
+	}
+	else
+		statusL = 0;          // nie zosta³ wciêniêty ¿aden klawisz 
+
+	if (btn == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+	{
+		statusP = 1;          // wciêniêty zosta³ lewy klawisz myszy
+		y_pos_old = y;         // przypisanie aktualnie odczytanej pozycji kursora 
+	}
+	else
+		statusP = 0;          // nie zosta³ wciêniêty ¿aden klawisz 
+
+}
+
+
+/*************************************************************************************/
+// Funkcja "monitoruje" po³o¿enie kursora myszy i ustawia wartoœci odpowiednich 
+// zmiennych globalnych
+
+void Motion(GLsizei x, GLsizei y)
+{
+
+	delta_x = x - x_pos_old;     // obliczenie ró¿nicy po³o¿enia kursora myszy
+
+	delta_y = y - y_pos_old;     // obliczenie ró¿nicy po³o¿enia kursora myszy
+
+	x_pos_old = x;            // podstawienie bie¿¹cego po³o¿enia jako poprzednie
+
+	y_pos_old = y;            // podstawienie bie¿¹cego po³o¿enia jako poprzednie
+
+	glutPostRedisplay();     // przerysowanie obrazu sceny
+}
+
+/*************************************************************************************/
+
+
+// Funkcja rysuj¹ca osie uk³adu wspó?rz?dnych
+
+/*void spinTeapot()
+{
+theta1[0] += 0.15;
+theta1[1] -= 0.25;
+theta1[2] += 0.05;
+glutPostRedisplay(); //odœwie¿enie zawartoœci aktualnego okna
+}
+*/
+
+void DrawEggTriangle()
+{
+	for (int i = 0; i <= N; i++)
+		for (int j = 0; j <= N; j++)
+		{
+			if (i <= (N - 1))
+			{
+				if (j <= (N - 1))
+				{
+					glBegin(GL_TRIANGLES);
+					glColor3f(kolory[i][j].x, kolory[i][j].y, kolory[i][j].z);
+					glVertex3f(tablica[i][j].x, tablica[i][j].y - 5.0f, tablica[i][j].z);
+					glColor3f(kolory[i + 1][j + 1].x, kolory[i + 1][j + 1].y, kolory[i + 1][j + 1].z);
+					glVertex3f(tablica[i + 1][j + 1].x, tablica[i + 1][j + 1].y - 5.0f, tablica[i + 1][j + 1].z);
+					glColor3f(kolory[i][j + 1].x, kolory[i][j + 1].y, kolory[i][j + 1].z);
+					glVertex3f(tablica[i][j + 1].x, tablica[i][j + 1].y - 5.0f, tablica[i][j + 1].z);
+					glEnd();
+					glBegin(GL_TRIANGLES);
+					glColor3f(kolory[i][j].x, kolory[i][j].y, kolory[i][j].z);
+					glVertex3f(tablica[i][j].x, tablica[i][j].y - 5.0f, tablica[i][j].z);
+					glColor3f(kolory[i + 1][j].x, kolory[i + 1][j].y, kolory[i + 1][j].z);
+					glVertex3f(tablica[i + 1][j].x, tablica[i + 1][j].y - 5.0f, tablica[i + 1][j].z);
+					glColor3f(kolory[i + 1][j + 1].x, kolory[i + 1][j + 1].y, kolory[i + 1][j + 1].z);
+					glVertex3f(tablica[i + 1][j + 1].x, tablica[i + 1][j + 1].y - 5.0f, tablica[i + 1][j + 1].z);
+					glEnd();
+				}
+			}
+			else
+			{
+				glBegin(GL_TRIANGLES);
+				glColor3f(kolory[i][j].x, kolory[i][j].y, kolory[i][j].z);
+				glVertex3f(tablica[i][j].x, tablica[i][j].y - 5.0f, tablica[i][j].z);
+				glColor3f(kolory[0][0].x, kolory[0][0].y, kolory[0][0].z);
+				glVertex3f(tablica[0][0].x, tablica[0][0].y - 5.0f, tablica[0][0].z);
+				glColor3f(kolory[i][0].x, kolory[i][0].y, kolory[i][0].z);
+				glVertex3f(tablica[i][0].x, tablica[i][0].y - 5.0f, tablica[i][0].z);
+				glEnd();
+
+				glBegin(GL_TRIANGLES);
+				glColor3f(kolory[i][j].x, kolory[i][j].y, kolory[i][j].z);
+				glVertex3f(tablica[i][j].x, tablica[i][j].y - 5.0f, tablica[i][j].z);
+				glColor3f(kolory[0][j].x, kolory[0][j].y, kolory[0][j].z);
+				glVertex3f(tablica[0][j].x, tablica[0][j].y - 5.0f, tablica[0][j].z);
+				glColor3f(kolory[0][0].x, kolory[0][0].y, kolory[0][0].z);
+				glVertex3f(tablica[0][0].x, tablica[0][0].y - 5.0f, tablica[0][0].z);
+				glEnd();
+			}
+		}
+}
+
+void GeneratingColors()
+{
+	kolory = new Point*[N + 1];
+	for (int i = 0; i <= N; i++)
+		kolory[i] = new Point[N + 1];
+
+	for (int i = 0; i <= N; i++)
+		for (int j = 0; j <= N; j++)
+		{
+
+			//	else {
+			kolory[i][j].x = (float)(rand() % 1000 / 1000.0);
+			kolory[i][j].y = (float)(rand() % 1000 / 1000.0);
+			kolory[i][j].z = (float)(rand() % 1000 / 1000.0);
+			//	}
+		}
+
+	for (int i = 0; i <= N; i++)
+		for (int j = 0; j <= N; j++)
+		{
+			if (j == N - 1)
+			{
+				kolory[i][j].x = kolory[i][0].x;
+				kolory[i][j].y = kolory[i][0].y;
+				kolory[i][j].z = kolory[i][0].z;
+			}
+		}
+
+
+
+	/*
+	for (int i = 0; i <= N; i++)
+	{
+	for(int j = 0 ; j <= N; j++)
+	std::cout << "X = " << kolory[i][j].x << "\t";
+	std::cout << std::endl;
+	for (int j = 0; j <= N; j++)
+	std::cout << "Y = " << kolory[i][j].y << "\t";
+	std::cout << std::endl;
+	for (int j = 0; j <= N; j++)
+	std::cout << "Z = " << kolory[i][j].z << "\t";
+	std::cout << std::endl << std::endl;
+	}
+	std::cin.get();
+	std::cin.get();*/
+	tablica = new Point*[N + 1];
+	for (int i = 0; i <= N; i++)
+		tablica[i] = new Point[N + 1];
+
+}
+
+void Egg()
+{
+	for (int i = 0; i <= N; i++)
+		for (int j = 0; j <= N; j++)
+		{
+			float u = (float)i / (float)N;
+			float v = (float)j / (float)N;
+			tablica[i][j].x = ((-90 * pow(u, 5) + 225 * pow(u, 4) - 270 * pow(u, 3) + 180 * u*u - 45 * u)*cos((float)M_PI*v));
+			tablica[i][j].y = (160 * pow(u, 4) - 320 * pow(u, 3) + 160 * u*u);
+			tablica[i][j].z = ((-90 * pow(u, 5) + 225 * pow(u, 4) - 270 * pow(u, 3) + 180 * u*u - 45 * u)*sin((float)M_PI*v));
+		}
+
+	/*for(int i = 0; i <= N; i++)
+	{
+	for (int j = 0; j <= N; j++)
+	std::cout << tablica[i][j].x << ", " << tablica[i][j].y << ", " << tablica[i][j].z << "\t";
+
+	std::cout << std::endl;
+	}
+
+	std::cin.get();
+	std::cin.get();
+	*/
+
+	DrawEggTriangle();
+
+
+}
+
+
+void Axes(void)
+{
+
+	point3  x_min = { -5.0, 0.0, 0.0 };
+	point3  x_max = { 5.0, 0.0, 0.0 };
+	// pocz?tek i koniec obrazu osi x
+
+	point3  y_min = { 0.0, -5.0, 0.0 };
+	point3  y_max = { 0.0,  5.0, 0.0 };
+	// pocz?tek i koniec obrazu osi y
+
+	point3  z_min = { 0.0, 0.0, -5.0 };
+	point3  z_max = { 0.0, 0.0,  5.0 };
+	//  pocz?tek i koniec obrazu osi y
+	glColor3f(1.0f, 0.0f, 0.0f);  // kolor rysowania osi - czerwony
+	glBegin(GL_LINES); // rysowanie osi x
+	glVertex3fv(x_min);
+	glVertex3fv(x_max);
+	glEnd();
+
+	glColor3f(0.0f, 1.0f, 0.0f);  // kolor rysowania - zielony
+	glBegin(GL_LINES);  // rysowanie osi y
+
+	glVertex3fv(y_min);
+	glVertex3fv(y_max);
+	glEnd();
+
+	glColor3f(0.0f, 0.0f, 1.0f);  // kolor rysowania - niebieski
+	glBegin(GL_LINES); // rysowanie osi z
+
+	glVertex3fv(z_min);
+	glVertex3fv(z_max);
+	glEnd();
+
+}
+
+/*************************************************************************************/
+
+// Funkcja okreœlaj¹ca co ma byæ rysowane (zawsze wywo³ywana, gdy trzeba 
+// przerysowaæ scenê)
+
+void viewerPlacing()
+{
+	viewer[0] = promien*cos(THETA)*cos(PHI);
+	viewer[1] = promien*sin(PHI);
+	viewer[2] = promien*sin(THETA)*cos(PHI);
+}
+
+void AnglesCounting()
+{
+	/*GLfloat temp1 = cosTheta, temp2 = sinTheta, temp3 = cosPhi, temp4 = sinPhi;
+	cosTheta = viewer[0] / (GLfloat)sqrt(viewer[0] * viewer[0] + viewer[1] * viewer[1]);
+	if (cosTheta < -1 || cosTheta > 1)
+		cosTheta = temp1;
+	sinTheta = viewer[2] / (GLfloat)sqrt(viewer[0] * viewer[0] + viewer[2] * viewer[2]);
+	if (sinTheta < -1 || cosTheta > 1)
+		sinTheta = temp1;
+	//cosPhi = (GLfloat)sqrt(viewer[0] * viewer[0] + viewer[2] * viewer[2]) /	(GLfloat)sqrt((GLfloat)sqrt(viewer[0] * viewer[0] + viewer[1] * viewer[1]) + viewer[1]*viewer[1]);
+	cosPhi = (GLfloat)sqrt(viewer[0] * viewer[0] + viewer[2] * viewer[2]) / promien;
+	if (cosPhi < -1 || cosTheta > 1)
+		cosPhi = temp1;
+//	sinPhi = viewer[1] / (GLfloat)sqrt((GLfloat)sqrt(viewer[0] * viewer[0] + viewer[1] * viewer[1]) + viewer[1] * viewer[1]);
+	sinPhi = viewer[1] /promien;
+
+	if (sinPhi < -1 || cosTheta > 1)
+		sinPhi = temp1;*/
+
+/*
+	GLfloat temp1 = PHI, temp2 = THETA;
+	if (PHI >= 0 && PHI <= M_PI)
+	PHI += delta_y*pix2angleY / 40.0;
+	if (PHI < 0 || PHI > 2 * M_PI)
+		PHI = temp1;
+	if (THETA >= 0 && THETA <= M_PI)
+	THETA += delta_x*pix2angleX / 40.0;
+	if (THETA < 0 || THETA > 2 * M_PI)
+		THETA = temp2;
+	*/
+
+	//GLfloat temp1 = PHI, temp2 = THETA;
+	//if(!(viewer[0] <= 0.5 && viewer[0] >= -0.5 && viewer[2] <= 0.5 && viewer[2] >= -0.5))
+	//{
+			//PHI += delta_y*pix2angleY / 20.0;
+			//THETA += delta_x*pix2angleX / 20.0;
+	//}
+	//viewerPlacing();
+	//if (viewer[0] <= 0.5 && viewer[0] >= -0.5 && viewer[2] <= 0.5 && viewer[2] >= -0.5)
+	//{
+	//	PHI = temp1;
+	//	THETA = temp2;
+	//}
+
+	/*if (viewer[0] == 0.0 && viewer[2] == 0.0)
+		if (kierunek == true)
+			kierunek = false;
+		else
+			kierunek = true;
+	
+	if(kierunek)
+	{
+		PHI += delta_y*pix2angleY / 40.0;
+		THETA += delta_x*pix2angleX / 40.0;
+	}
+	else
+	{
+		PHI = PHI * (-1.0);
+		THETA = THETA * (-1.0);
+	}
+	*/
+	
+	PHI += delta_y*pix2angleY;// / 40.0;
+//	PHI = fmod(PHI, M_PI);
+	THETA += delta_x*pix2angleX;// / 40.0;
+//	THETA = fmod(THETA, M_PI);
+
+	if (cosf(PHI) >= 0.0f)
+		y = 1.0f;
+	else
+		y = -1.0f;
+
+}
+
+
+
+void RenderScene(void)
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Czyszczenie okna aktualnym kolorem czyszcz¹cym
+
+	glLoadIdentity();
+	// Czyszczenie macierzy bie??cej
+
+
+
+	//gluLookAt(5.0, 2.0, 10.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0);
+	gluLookAt(viewer[0], viewer[1], viewer[2], 0.0, 0.0, 0.0, 0.0, y, 0.0);
+	// Zdefiniowanie po³o¿enia obserwatora
+	Axes();
+	// Narysowanie osi przy pomocy funkcji zdefiniowanej powy¿ej
+
+
+/*
+	if (statusL == 1)                     // jeœli lewy klawisz myszy wciêniêty
+	{
+		theta[0] += delta_x*pix2angleX;
+		theta[1] += delta_y*pix2angleY;
+
+	}                                  // do ró¿nicy po³o¿eñ kursora myszy
+
+	if (statusP == 1)                     // jeœli prawy klawisz myszy wciêniêty
+	{
+		GLfloat temp = viewer[2];
+		if (viewer[2] >= 7.0 && viewer[2] <= 30.0)
+			viewer[2] += delta_y*pix2angleY;    // modyfikacja k¹ta obrotu o kat proporcjonalny
+		if (viewer[2] < 7.0 || viewer[2] > 30.0)
+			viewer[2] = temp;
+	}
+	*/
+
+	if (statusL == 1)                     // jeœli lewy klawisz myszy wciêniêty
+	{
+		AnglesCounting();
+	}                                  // do ró¿nicy po³o¿eñ kursora myszy
+
+	if (statusP == 1)                     // jeœli prawy klawisz myszy wciêniêty
+	{
+		GLfloat temp = promien;
+		if (promien >= 7.0 && promien <= 30.0)
+			promien += delta_y*pix2angleY;    // modyfikacja k¹ta obrotu o kat proporcjonalny
+		if (promien < 7.0 || promien > 30.0)
+			promien = temp;
+	}
+
+	viewerPlacing();
+
+	//glRotatef(theta[0], 0.0, 1.0, 0.0);  //obrót obiektu o nowy k¹t
+	//glRotatef(theta[1], 1.0, 0.0, 0.0);  //obrót obiektu o nowy k¹t
+
+	/*
+	glRotatef(theta1[0], 1.0, 0.0, 0.0);
+	glRotatef(theta1[1], 0.0, 1.0, 0.0);
+	glRotatef(theta1[2], 0.0, 0.0, 1.0);
+	*/
+
+	//glColor3f(1.0f, 1.0f, 1.0f);
+	// Ustawienie koloru rysowania na bia³y
+
+	//glutWireTeapot(3.0);
+
+	Egg();
+	//glColor3f(1.0f, 1.0f, 1.0f);
+	//glutSolidTeapot(3.0f);
+
+	// Narysowanie czajnika
+	glFlush();
+	// Przekazanie poleceñ rysuj¹cych do wykonania
+	glutSwapBuffers();
+}
+/*************************************************************************************/
+
+// Funkcja ustalaj¹ca stan renderowania
+
+
+
+void MyInit(void)
+{
+	/************************************************************************************* /
+
+		//  Definicja materia³u z jakiego zrobiony jest czajnik 
+		//  i definicja Ÿród³a œwiat³a
+
+		/*************************************************************************************/
+
+
+		/*************************************************************************************/
+		// Definicja materia³u z jakiego zrobiony jest czajnik 
+
+		GLfloat mat_ambient[] = { 1.0, 1.0, 1.0, 1.0 };
+	// wspó³czynniki ka =[kar,kag,kab] dla œwiat³a otoczenia
+
+	GLfloat mat_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+	// wspó³czynniki kd =[kdr,kdg,kdb] œwiat³a rozproszonego
+
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	// wspó³czynniki ks =[ksr,ksg,ksb] dla œwiat³a odbitego                
+
+	GLfloat mat_shininess = { 20.0 };
+	// wspó³czynnik n opisuj¹cy po³ysk powierzchni
+
+
+	/*************************************************************************************/
+	// Definicja Ÿród³a œwiat³a
+
+
+	GLfloat light_position[] = { 0.0, 0.0, 10.0, 1.0 };
+	// po³o¿enie Ÿród³a
+
+
+	GLfloat light_ambient[] = { 0.1, 0.1, 0.1, 1.0 };
+	// sk³adowe intensywnoœci œwiecenia Ÿród³a œwiat³a otoczenia
+	// Ia = [Iar,Iag,Iab]
+
+	GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+	// sk³adowe intensywnoœci œwiecenia Ÿród³a œwiat³a powoduj¹cego
+	// odbicie dyfuzyjne Id = [Idr,Idg,Idb]
+
+	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	// sk³adowe intensywnoœci œwiecenia Ÿród³a œwiat³a powoduj¹cego
+	// odbicie kierunkowe Is = [Isr,Isg,Isb]
+
+	GLfloat att_constant = { 1.0 };
+	// sk³adowa sta³a ds dla modelu zmian oœwietlenia w funkcji 
+	// odleg³oœci od Ÿród³a
+
+	GLfloat att_linear = { 0.05f };
+	// sk³adowa liniowa dl dla modelu zmian oœwietlenia w funkcji 
+	// odleg³oœci od Ÿród³a
+
+	GLfloat att_quadratic = { 0.001f };
+	// sk³adowa kwadratowa dq dla modelu zmian oœwietlenia w funkcji
+	// odleg³oœci od Ÿród³a
+
+	/*************************************************************************************/
+	// Ustawienie parametrów materia³u i Ÿród³a œwiat³a
+
+	/*************************************************************************************/
+	// Ustawienie patrametrów materia³u
+
+
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess);
+
+
+	/*************************************************************************************/
+	// Ustawienie parametrów Ÿród³a
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, att_constant);
+	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, att_linear);
+	glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, att_quadratic);
+
+
+	/*************************************************************************************/
+	// Ustawienie opcji systemu oœwietlania sceny 
+
+	glShadeModel(GL_SMOOTH); // w³aczenie ³agodnego cieniowania
+	glEnable(GL_LIGHTING);   // w³aczenie systemu oœwietlenia sceny 
+	glEnable(GL_LIGHT0);     // w³¹czenie Ÿród³a o numerze 0
+	glEnable(GL_DEPTH_TEST); // w³¹czenie mechanizmu z-bufora 
+
+							 /*************************************************************************************/
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	// Kolor czyszcz¹cy (wype³nienia okna) ustawiono na czarny
+
+}
+
+/*************************************************************************************/
+
+
+// Funkcja ma za zadanie utrzymanie sta³ych proporcji rysowanych 
+// w przypadku zmiany rozmiarów okna.
+// Parametry vertical i horizontal (wysokoœæ i szerokoœæ okna) s¹ 
+// przekazywane do funkcji za ka¿dym razem gdy zmieni siê rozmiar okna.
+
+
+
+void ChangeSize(GLsizei horizontal, GLsizei vertical)
+{
+
+	pix2angleX = 2 * M_PI / (float)horizontal;  // przeliczenie pikseli na stopnie
+	pix2angleY = 2 * M_PI / (float)vertical;  // przeliczenie pikseli na stopnie
+
+	glMatrixMode(GL_PROJECTION);
+	// Prze³¹czenie macierzy bie¿¹cej na macierz projekcji
+
+	glLoadIdentity();
+	// Czyszcznie macierzy bie¿¹cej 
+
+	gluPerspective(70, 1.0, 1.0, 30.0);
+	// Ustawienie parametrów dla rzutu perspektywicznego
+
+
+	if (horizontal <= vertical)
+		glViewport(0, (vertical - horizontal) / 2, horizontal, horizontal);
+
+	else
+		glViewport((horizontal - vertical) / 2, 0, vertical, vertical);
+	// Ustawienie wielkoœci okna okna widoku (viewport) w zale¿noœci
+	// relacji pomiêdzy wysokoœci¹ i szerokoœci¹ okna
+
+	glMatrixMode(GL_MODELVIEW);
+	// Prze³¹czenie macierzy bie¿¹cej na macierz widoku modelu  
+
+	glLoadIdentity();
+	// Czyszczenie macierzy bie¿¹cej 
+
+}
+
+/*************************************************************************************/
+
+// G³ówny punkt wejœcia programu. Program dzia³a w trybie konsoli
+
+
+
+void main(void)
+{
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+	glutInitWindowSize(300, 300);
+
+	glutCreateWindow("Rzutowanie perspektywiczne - 2 Zadanie");
+
+	GeneratingColors();
+
+	glutDisplayFunc(RenderScene);
+	// Okreœlenie, ¿e funkcja RenderScene bêdzie funkcj¹ zwrotn¹
+	// (callback function).  Bêdzie ona wywo³ywana za ka¿dym razem 
+	// gdy zajdzie potrzeba przerysowania okna
+
+	glutMouseFunc(Mouse);
+	// Ustala funkcjê zwrotn¹ odpowiedzialn¹ za badanie stanu myszy
+
+	glutMotionFunc(Motion);
+	// Ustala funkcjê zwrotn¹ odpowiedzialn¹ za badanie ruchu myszy
+
+	//glutIdleFunc(spinTeapot);
+
+	glutReshapeFunc(ChangeSize);
+	// Dla aktualnego okna ustala funkcjê zwrotn¹ odpowiedzialn¹
+	// za zmiany rozmiaru okna                       
+
+
+	MyInit();
+	// Funkcja MyInit() (zdefiniowana powy¿ej) wykonuje wszelkie
+	// inicjalizacje konieczne  przed przyst¹pieniem do renderowania
+	glEnable(GL_DEPTH_TEST);
+	// W³¹czenie mechanizmu usuwania niewidocznych elementów sceny
+
+	glutMainLoop();
+	// Funkcja uruchamia szkielet biblioteki GLUT
+}
+
+/*************************************************************************************/
